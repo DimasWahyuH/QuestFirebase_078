612393d0a5ff597c1ce8b17632b019dd0451a36f
dimas <dimaswh19@gmail.com>
2026-01-08 13:43:24 +0700

    Menambahkan fungsi getSatuSiswa pada RepositorySiswa

--- app/src/main/java/com/example/questfirebase_078/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/questfirebase_078/repositori/RepositorySiswa.kt
@@ -7,6 +7,10 @@ import kotlinx.coroutines.tasks.await
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
+
+    suspend fun getSatuSiswa(id : Long): Siswa?
+
+
 }
 class FirebaseRepositorySiswa : RepositorySiswa {
     private val db = FirebaseFirestore.getInstance()
@@ -38,6 +42,26 @@ class FirebaseRepositorySiswa : RepositorySiswa {
         )
         docRef.set(data).await()
     }
-}
+
+    override suspend fun getSatuSiswa(id: Long): Siswa? {
+        return try {
+            val query = collection.whereEqualTo("id", id).get().await()
+            query.documents.firstOrNull()?.let { doc ->
+                Siswa(
+                    id = doc.getLong("id")?.toLong() ?: 0,
+                    nama = doc.getString("nama") ?: "",
+                    alamat = doc.getString("alamat") ?: "",
+                    telpon = doc.getString("telpon") ?: ""
+                )
+            }
+        } catch (e: Exception) {
+            println("Gagal baca data siswa : ${e.message}")
+            null
+        }
+    }
+
+
+
+
 
 
