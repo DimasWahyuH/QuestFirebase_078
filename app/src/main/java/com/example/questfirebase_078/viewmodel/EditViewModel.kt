927c74c620d3d6c68e103c4cc2c31cd57d4b8097
dimas <dimaswh19@gmail.com>
2026-01-08 13:41:46 +0700

    Menambahkan fungsi validasiInput pada EditViewModel

--- app/src/main/java/com/example/questfirebase_078/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_078/viewmodel/EditViewModel.kt
@@ -33,3 +33,9 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySi
         uiStateSiswa =
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
