ab125097091bb069909108d4c13b019ff5f4f4a0
dimas <dimaswh19@gmail.com>
2026-01-08 13:38:49 +0700

    Menambahkan interface StatusUIDetail pada DetailViewModel

--- app/src/main/java/com/example/questfirebase_078/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_078/viewmodel/DetailViewModel.kt
@@ -1,1 +1,19 @@
 package com.example.questfirebase_078.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_078.modeldata.Siswa
+import com.example.questfirebase_078.repositori.RepositorySiswa
+import com.example.questfirebase_078.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
